c90315d0d7ae5af8ba61b7b7689ef368676286e1
Judah Himango <Judah.Himango@microsoft.com>
2023-04-20 14:34:15 -0700

    Adds Nuget version incrementer powershell script

---
+++ GetNextNugetPackageVersion.psm1
@@ -0,0 +1,14 @@
+Function GetNextNugetPackageVersion {
+
+	$json = Invoke-RestMethod -Uri "https://api.nuget.org/v3/registration5-semver1/ravendb.identity/index.json"
+	$package = $json.items[0]
+	$latestPackageIndex = $package.items.Length - 1;
+	$latestPackage = $package.items[$latestPackageIndex]
+	$versionStr = $latestPackage.catalogEntry.version
+	
+	# Now that we have the version string ("8.0.7" or whatever), parse that and increment the Build part of it ("7" becomes "8" in this case)
+	$versionObj = [version]::Parse($versionStr)
+	$buildNumber = $versionObj.Build	
+	
+    return $buildNumber + 1
+}
